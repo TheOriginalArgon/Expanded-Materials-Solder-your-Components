db186270b4c1a14a32c7703599c4c1a20791e8d6
Argón <argon5571@gmail.com>
2023-12-03 14:11:06 -0300

    Initial commit

---
+++ Source/SolderComponents/HarmonyPatches.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using HarmonyLib;
+
+namespace SolderComponents
+{
+    [StaticConstructorOnStartup]
+    public class Patcher
+    {
+        static Patcher()
+        {
+            var harmony = new Harmony("Argon.EM.SYC");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(WorkGiver_FixBrokenDownBuilding))]
+    [HarmonyPatch("FindClosestComponent")]
+    class Patch_ComponentRepair
+    {
+        static void Postfix(Pawn pawn, ref Thing __result)
+        {
+            __result = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForDef(DefDatabase<ThingDef>.GetNamed("EM_RepairKit")), PathEndMode.InteractionCell, TraverseParms.For(pawn, pawn.NormalMaxDanger()), 9999f, (Thing x) => !x.IsForbidden(pawn) && pawn.CanReserve(x));
+        }
+    }
+}
